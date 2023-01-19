3a21cc533cbe440e7c628e6963e89fb13b408ef1
tokunagatensei <tensei.soccer14@icloud.com>
2023-01-19 18:37:44 +0900

    メッセージ送信機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2023_01_19_053058) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "confirmation_sent_at"
+    t.string "content"
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
